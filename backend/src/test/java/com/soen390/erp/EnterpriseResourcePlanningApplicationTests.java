89f9860563def0f533a423c49542e657bd89c534
Zubair Nurie <mznurie@msn.com>
2021-02-02 05:04:46 -0500

    Update security config file

--- backend/src/test/java/com/soen390/erp/EnterpriseResourcePlanningApplicationTests.java
+++ backend/src/test/java/com/soen390/erp/EnterpriseResourcePlanningApplicationTests.java
@@ -1,13 +1,24 @@
 package com.soen390.erp;
 
+//import com.soen390.erp.users.service.UserCreationService;
+import com.soen390.erp.users.service.UserCreationService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EnterpriseResourcePlanningApplicationTests {
 
+    @Autowired
+    UserCreationService userCreationService;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void createUser(){
+//        userCreationService.createUser("Zubair", "Nurie", "mznurie@msn.com", "soen390", "ADMIN");
     }
 
 }
