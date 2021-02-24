8f0be0523206e4a6c7855729b4d3be5c44ea767f
Stella Nguyen <stellakimnguyen@gmail.com>
2021-02-23 21:54:25 -0500

    Add dropdown component

---
+++ frontend/src/components/CustomDropdown.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import styled from 'styled-components';
+
+class FieldContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <Container name={this.props.dropdownName} id={this.props.dropdownID}>
+            {this.props.children}
+        </Container>
+    );
+  }
+}
+
+//STYLED-COMPONENTS
+const Container = styled.select`
+  background: white;
+  padding: 7px;
+  width: 250px;
+  border-radius: 8px;
+  margin-bottom: 5px;
+  border: solid 2px #BBC8E3;
+  display: flex;
+  flex-direction: column;
+
+  font-family: Montserrat;
+  font-size: 9pt;
+  color: '#556C99';
+  text-transform: lowercase;
+  letter-spacing: 0.2em;
+  font-weight: 500;
+`
+
+FieldContainer.propTypes = {
+    children: PropTypes.element.isRequired,
+};
+
+export default FieldContainer;
