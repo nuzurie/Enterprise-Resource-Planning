a419bbb38b42ebaec6ebe8465c640e67cb3aa05c
Zubair Nurie <mznurie@msn.com>
2021-03-02 01:43:08 -0500

    ERPS-41 Add redirect after login successful

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -29,7 +29,7 @@ class Login extends Component {
 
           }
       })
-    .then(this.props.history.push('/dashboard'))
+    .then(() => this.props.history.push('/dashboard'))
     .catch(err => console.log(err))
 
     // axios.get('/asd', {
