717125242d71edf6bc10a656502d825783812924
Ayman <aymalsh@gmail.com>
2021-03-07 20:23:09 +0300

    add SaleOrder to Entity

--- backend/src/main/java/com/soen390/erp/accounting/model/SaleOrder.java
+++ backend/src/main/java/com/soen390/erp/accounting/model/SaleOrder.java
@@ -1,11 +1,22 @@
 package com.soen390.erp.accounting.model;
 
+import lombok.*;
+
+import javax.persistence.*;
 import java.util.Date;
 
 
 //TODO: should be merged with ClientOrder that Carlin did
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class SaleOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private Date date;
     private int plant_id;
