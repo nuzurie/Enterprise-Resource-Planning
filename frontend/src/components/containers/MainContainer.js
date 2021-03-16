2032e5288b6c66493cd32a759d52fcae3a6de142
Stella Nguyen <stellakimnguyen@gmail.com>
2021-03-15 21:09:58 -0400

    Fix font display 3.0

--- frontend/src/components/containers/MainContainer.js
+++ frontend/src/components/containers/MainContainer.js
@@ -72,7 +72,7 @@ const Header = styled.div`
 `
 
 const Title = styled.div`
-    font-family: Proxima Nova;
+    // font-family: Proxima Nova;
     font-size: 12pt;
     color: black;
     text-transform: uppercase;
