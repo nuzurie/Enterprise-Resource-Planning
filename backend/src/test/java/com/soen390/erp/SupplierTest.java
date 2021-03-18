2eab9cc9f6fe88b06ab27ca61f844487740c1a48
carlin lee <car12lin12@gmail.com>
2021-03-17 21:11:38 -0400

    remove useless spy annotation

--- backend/src/test/java/com/soen390/erp/SupplierTest.java
+++ backend/src/test/java/com/soen390/erp/SupplierTest.java
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SupplierTest {
-    @Spy
+
     @InjectMocks
     private SupplierController supplierController;
 
