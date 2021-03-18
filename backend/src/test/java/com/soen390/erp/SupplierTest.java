819d748414fdac8c64ea1a057c5b50ae15f67a9e
carlin lee <car12lin12@gmail.com>
2021-03-17 23:10:36 -0400

    account controller tests

--- backend/src/test/java/com/soen390/erp/SupplierTest.java
+++ backend/src/test/java/com/soen390/erp/SupplierTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
