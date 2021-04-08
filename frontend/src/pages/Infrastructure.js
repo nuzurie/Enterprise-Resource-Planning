6954d761daa87ed1d1f95007a006cfe74fb30d03
vithura14 <vithura14@hotmail.com>
2021-04-07 20:36:47 -0400

    Add the radio buttons

--- frontend/src/pages/Infrastructure.js
+++ frontend/src/pages/Infrastructure.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styled from 'styled-components';
 import MainContainer from '../components/containers/MainContainer.js';
+import InnerContainer from '../components/containers/InnerContainer.js';
+import CustomRadioButton from '../components/CustomRadioButton.js';
 
 class Infrastructure extends Component {
   constructor(props) {
@@ -11,8 +13,16 @@ class Infrastructure extends Component {
   render() {
     return (
         <MainContainer title="Infrastructure">
-            
+          <InnerContainer title="Logistics">
+            <div>
+              <CustomRadioButton value="Inventory">Inventory</CustomRadioButton>
+              <CustomRadioButton value="Manufacturing">Manufacturing</CustomRadioButton>
+              <CustomRadioButton value="Accounting">Accounting</CustomRadioButton>
+              <CustomRadioButton value="All">All</CustomRadioButton>
+            </div>
+          </InnerContainer>
         </MainContainer>
+        
     );
   }
 }
